bde741a1d54a84a08942236601fde43961772794
Anup Adhikari <developeradhikari@gmail.com>
2023-06-15 15:52:19 +0000

    Define of module function

---
+++ 10.module.py
@@ -0,0 +1,19 @@
+#module is pre defined package where it consist of function
+
+import math
+a=math.sqrt(16)#square root of 16
+print(a)
+
+print(dir(math)) #to find the function of math
+
+print(math.pi)
+print(math.sin(60))
+print(math.factorial(2))
+
+import calendar
+
+cal=calendar.month(2023,6)# print calender of this month
+print(cal)
+
+print(calendar.isleap(2024)) #leap year
+
