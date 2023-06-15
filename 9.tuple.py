1b4fab7dafe20b6352647e34ceadf55b70bdd07d
Anup Adhikari <developeradhikari@gmail.com>
2023-06-15 13:22:23 +0000

    python tuple

---
+++ 9.tuple.py
@@ -0,0 +1,13 @@
+#Tuple is a list of value that is grouped together
+
+point=(5,9)
+print(point[1])
+
+#the difference between list and tuple is that All values have similar meaning in list
+#but in Tuple All values have different meaning
+
+axis=(5,4)
+addr=("1 Purple Street","Newyork",10001)
+print("The x-axis point is ",axis[0])
+print("The full address is",addr)
+
