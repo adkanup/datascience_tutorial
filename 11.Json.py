4d13f8c2c77307b10688d7e356dfb2b864e63c97
Anup Adhikari <developeradhikari@gmail.com>
2023-06-16 04:24:42 +0000

    Define of JSON

---
+++ 11.Json.py
@@ -0,0 +1,27 @@
+#JSON - Java Script object Notation
+
+#JSon is a data exchange format which is similar to XML
+
+#Json is a light weight format compared to XML
+
+# * To create address book and write some records into it
+# * Read this address book
+
+book={}
+book['tom']={
+    'name':'tom',
+    'address':'1 Street, NY',
+    'phone':'9804857308'
+
+}
+book['alex']={
+    'name':'alex',
+    'address':'Texas',
+    'phone':'9806878508'
+}
+import json
+s=json.dumps(book)
+print(s)
+with open("book.txt","w") as f: #add your file location 
+    f.write(s)
+
